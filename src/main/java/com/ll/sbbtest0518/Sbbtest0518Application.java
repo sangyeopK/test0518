88b5999bbfcb87b1bfcfa8d20dad65a07a1c4f3f
sangyeopK <creativethony@naver.com>
2023-05-18 12:16:41 +0900

    시작

---
+++ src/main/java/com/ll/sbbtest0518/Sbbtest0518Application.java
@@ -0,0 +1,13 @@
+package com.ll.sbbtest0518;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbbtest0518Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbbtest0518Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/sbbtest0518/Sbbtest0518ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sbbtest0518;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbbtest0518ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
